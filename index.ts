e1eb9ac87728e67b3e1383f6fc9267bed4be637d
john.muthurimi <john.muthurimi@softwaregroup-bg.com>
2017-08-17 16:37:39 +0300

    1.1.5

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-export * from './src/index';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HttpService } from './src/services/http.service';
+import { UtilitiesService } from './src/services/utilities.service';
+
+export * from './src/services/http.service';
+export * from './src/services/utilities.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule {
+  static forRoot(getConfig: Function): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        HttpService,
+        UtilitiesService
+      ]
+    };
+  }
+}
